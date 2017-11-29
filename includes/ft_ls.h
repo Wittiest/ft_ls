7dc7676f494c8718ac7917cc5de3dc50260f972b
Wittiest <dustinparkerpearson@gmail.com>
2017-11-29 04:12:51 -0800

    Linked Lists of directories and items

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -24,4 +24,34 @@
 # include <time.h>
 # include <uuid/uuid.h>
 
+typedef struct	s_flags
+{
+	int flag_l;
+	int flag_a;
+	int flag_R:
+	int	flag_r;
+	int	flag_t;
+}				t_flags;
+
+/*
+**	A lexicographically sorted list of the data we will print for each item
+**	or directory.
+*/
+typedef struct			s_list_lex
+{
+	char				*sort_name;
+	char				*print_data;
+	struct s_list_hex	*next;
+}						t_list;
+
+/*
+**	Holds the head of LL of items and head of LL of directories. The items get
+**	printed first.
+*/
+typedef	struct	s_dir_item
+{
+	t_list	*item_head;
+	t_list	*dir_head;
+}				t_dir_item;
+
 #endif
--- srcs/ft_ls.c
+++ srcs/ft_ls.c
@@ -42,15 +42,79 @@ void	list_files(const char *filename)
 	while ((file = readdir(dir)))
 		list_file_data(file->d_name);
 }
+/*
+**	Currently done with if statements, later create an enum and jump table.
+**	the -g flag overrides the -l flag regardless of order.
+*/
+void	flag_check(char c, t_flags *flags)
+{
+	if (c == 'l')
+		flags->l = 1;
+	else if (c == 'a')
+		flags->a = 1;
+	else if (c == 'r')
+		flags->r = 1;
+	else if (c == 'R')
+		flags->R = 1;
+	else if (c == t)
+		flags->t = 1;
+}
 
+int		parse_flags(t_flags *flags, int argc, char **argv)
+{
+	int i;
+	int j;
+
+	if (argc < 2)
+		return ;
+	i = 1;
+	while (i < argc)
+	{
+		if (argv[i][0] != '-')
+			return (i);
+		j = 1;
+		while (argv[i][j])
+		{
+			flag_check(argv[i][j], flags);
+			j++;
+		}
+		i++;
+	}
+	return (i);
+}
+
+/*	I need a function that can store all of the data in a directory, using a LL
+**	It will iterate through the LL, comparing the directory/filename and placing
+**	its arg lexicographically.
+*/
+
+/*
+**	parse_args is given the end of the argument vector to iterate through.
+**	It will identify the directories and files listed after the ls flags,
+**	first displaying the non-directorys lexicographically, then the same with
+**	the directories.
+*/
+void	parse_args(t_flags *flags, int argc, int i, char **argv)
+{
+	if (i >= argc)
+		printf("CASE OF NO ARGUMENTS == PRINT . DIRECTORY FILES with flags\n")
+	while (i < argc)
+	{
+		list_files(argv[i]);
+		i++;
+	}
+}
+
+/*
+	If the modification time of the file is more than 6 months in the past or future, then the year
+	of the last modification is displayed in place of the hour and minute fields.
+*/
 int		main(int argc, char **argv)
 {
-	// parsing with usage statement
-	// flags -l (multiple column format), -R, -a , -r, -t
-	// flag -l always overrides -g
-	if (argc == 1)
-		list_files(".");
-	if (argc == 2)
-		list_files(argv[1]);
+	t_flags		flags;
+	int			i;
+
+	i = parse_flags(&flags, argc, argv)
+	parse_args(&flags, argc, i, argv);
 	return (0);
 }
